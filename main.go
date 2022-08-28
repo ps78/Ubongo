9a76c0899faaae255dc62a85e827da8f6ed1bf59
Pascal Simon <mail@pascalsimon.com>
2022-08-28 21:40:41 +0200

    added volume

--- main.go
+++ main.go
@@ -5,19 +5,26 @@ import (
 )
 
 type Block struct {
-	name  string
-	cubes [][][]bool
+	Name  string
+	Cubes [][][]bool
+}
+
+type Volume struct {
+	Shape  [][][]int8 // 0=not part of volume, 1=empty, 2=occupied by a block
+	Height int
 }
 
 func makeBlocks() []Block {
-	var blocks = make([]Block, 3)
-	blocks[0] = Block{"L", [][][]bool{{{true, false}, {true, false}, {true, true}}}}
-	blocks[1] = Block{"v", [][][]bool{{{true, false}, {true, true}}}}
-	blocks[2] = Block{"M", [][][]bool{{{true, false}, {true, true}, {true, false}}}}
+	var blocks = make([]Block, 4)
+	blocks[0] = Block{Name: "01", Cubes: [][][]bool{{{false, true}, {true, true}, {false, true}}, {{false, true}, {false, false}, {false, false}}}}
+	blocks[1] = Block{Name: "02", Cubes: [][][]bool{{{true, true}, {false, true}, {false, true}}, {{false, false}, {false, false}, {false, true}}}}
+	blocks[2] = Block{Name: "03", Cubes: [][][]bool{{{true, true}, {true, false}}, {{false, true}, {false, false}}}}
+	blocks[3] = Block{Name: "04", Cubes: [][][]bool{{{true, true}, {false, true}, {true, true}}}}
 	return blocks
 }
 
 func main() {
 	b := makeBlocks()
-	fmt.Println(b)
+	v := Volume{Height: 3, Shape: [][][]int8{{{0, 1, 1, 1, 0}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 0}, {1, 1, 1, 0, 0}}, {{0, 1, 1, 1, 0}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 0}, {1, 1, 1, 0, 0}}}}
+	fmt.Println(b[0], v)
 }
